bdf52386c764397331054d1322d8bffdcef875ad
Nebulina <139708191+Nebulina@users.noreply.github.com>
2023-09-09 20:02:47 -0400

    week 9 update

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,23 +1,140 @@
 package projects;
 
-//1.   Create a class in the projects package named ProjectsApp. The class must have a main() method.
-//2.   In the main() method, call the DbConnection.getConnection() method.
-
-
-import java.sql.Connection;
-
-import projects.dao.DbConnection;
+import java.math.BigDecimal;
+import projects.entity.Project;
+import projects.exception.DbException;
+import projects.service.ProjectService;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class ProjectsApp {
 
-	public static void main(String[] args) {
-		DbConnection.getConnection();
+	private Scanner scanner = new Scanner(System.in); // Scanner instance
+	private ProjectService projectService = new ProjectService();
+
+	// @formatter:off
+		private List<String> operations = List.of( 
+				"1) Add a project" 
+				);
+		// @formatter:on
+
+	public static void main(String[] args) { //MAIN
+
+		new ProjectsApp().processUserSelections();
 	}
 
-	
-		 
+	private void processUserSelections() {// This method displays selection, gathers user input and then acts
+		boolean done = false;
+
+		while (!done) { //Selection Options Process
+			try {
+				int selection = getUserSelection();
+
+				switch (selection) {
+				case -1:
+					done = exitMenu();
+					break;
+
+				case 1:
+					createProject();
+					break;
+
+				default:
+					System.out.println("\n" + selection + "is not a valid selection. Try again.");
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println("\nError: " + e + " Try again.");
+				
+			}
+		}
+
+	}
+
 
 	
+
+	private void createProject() { //Resulting options once valid input received.
+		String projectName = getStringInput("Enter the project name");
+		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
+		BigDecimal actualHours = getDecimalInput("Enter the actual hours");
+		Integer difficulty = getIntInput("Enter the project difficulty (1-5)");
+		String notes = getStringInput("Enter the project notes");
+
+		Project project = new Project(); //Sets the resulting options.
+
+		project.setProjectName(projectName);
+		project.setEstimatedHours(estimatedHours);
+		project.setActualHours(actualHours);
+		project.setDifficulty(difficulty);
+		project.setNotes(notes);
+		
+		Project dbProject = projectService.addProject(project);
+		System.out.println("You have successfully created project: " + dbProject);
+		
+	}
+
+	private BigDecimal getDecimalInput(String prompt) {
+		String input = getStringInput(prompt);
+
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		try {
+
+			return new BigDecimal(input).setScale(2);
+
+		} catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number."); //BigDecimal method
+		}
+	}
+
+	private boolean exitMenu() {
+		System.out.println("Exiting the menu."); //Exiting the menu method
+		return true;
+	}
 	
 	
+	private int getUserSelection() { // Will print the operations and accept user input an integer
+		printOperations();
+
+		Integer input = getIntInput("Enter a menu selection");
+		
+		return Objects.isNull(input) ? -1 : input;
+	}
+
+	private Integer getIntInput(String prompt) {
+		String input = getStringInput(prompt);
+		
+		if (Objects.isNull(input)) {
+		      return null;
+		    }
+	
+    try {
+        return Integer.valueOf(input);
+      } 
+    catch (NumberFormatException e) {
+        throw new DbException(input + " is not a valid number.");
+      }
+	}
+
+	private String getStringInput(String prompt) {
+		System.out.println(prompt + ": ");
+		String input = scanner.nextLine();
+		
+		return input.isBlank() ? null : input.trim();
+	}
+
+	private void printOperations() {
+		System.out.println("\nThese are the available selections. Press the Enter key to quit:"); //Print the operations.
+		
+		operations.forEach(input -> System.out.println("   " + input));
+	
+
+	
+	}		 
+
 }
+
+
